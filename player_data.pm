88638c0341abdbc7c2822fd2f65361550ff4d2b3
jim <jamespmarsden@hotmail.com>
2019-06-01 08:10:00 -0700

    added healing, and max health

--- player_data.pm
+++ player_data.pm
@@ -3,16 +3,18 @@ use strict;
 use warnings FATAL => 'all';
 
 use Exporter qw(import);
-our @EXPORT_OK = qw(add set_health do_damage);
+our @EXPORT_OK = qw(add set_health do_damage heal);
 
 
 my $name;
 my $health;
+my $max_health;
 
 BEGIN{
 
     $name = "Swifty";
     $health = 100;
+    $max_health = 200;
     print "Player Data being init'd with name($name) and health($health)\n";
 }
 
@@ -29,6 +31,16 @@ sub do_damage
     if($health < 0)
     {
         $health = 0;
+    }
+    return $health;
+}
+
+sub heal
+{
+    $health += $_[0];
+    if($health > $max_health)
+    {
+        $health = $max_health;
     }
     return $health;
 }
