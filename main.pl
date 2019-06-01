bbe1dd46ecddc1307a8b8737bab443bd9f287ff5
jim <jamespmarsden@hotmail.com>
2019-06-01 08:09:09 -0700

    Changed the various health tests into one sub

--- main.pl
+++ main.pl
@@ -4,6 +4,12 @@ use strict;
 
 use lib '.';
 require player_data;
+
+BEGIN
+{
+    print "test!\n";
+}
+
 sub init()
 {
 
@@ -13,27 +19,20 @@ sub init()
 
 }
 
-sub health_tests
+sub health_test
 {
-    my $damage_to_deal = 55;
-    print 
-        "Staring Player Health (" . Player_Data::get_health() . ")" . "\n"
-        . "Amount being delt   ($damage_to_deal)" . "\n"
-        . "ending amount       (" . Player_Data::do_damage($damage_to_deal) . ")\n";
-         #"health before damage: " . Player_Data::get_health() . " and damage "
-        #. "damage delt will be $damage_to_deal and the final health is " 
-        #. Player_Data::do_damage($damage_to_deal) . "\n\n";
-
-    
-    #print "health is " . Player_Data::get_health() . "\n
-    #damage_to_deal = $damage_to_deal\n health after: " . Player_Data::do_damage($damage_to_deal) . "\n" .
-    #" " . Player_Data::get_health();
-
+    my ($amount, $function) = @_;
+    print
+          "Staring Player Health (" . Player_Data::get_health() . ")" . "\n"
+        . "Amount being delt ... ($amount)" . "\n"
+        . "ending amount ....... (" . $function->($amount) . ")\n\n";
 }
 
+health_test(44, *Player_Data::heal);
 
-health_tests();
-health_tests();
+health_test(100, *Player_Data::do_damage);
+
+
 
 my $rv = init();
 if ($rv == 0) {
@@ -42,5 +41,6 @@ if ($rv == 0) {
 
 else
 {
+
     print "something went wrong\n";
 }
